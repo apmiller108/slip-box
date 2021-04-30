3fb07f58ee5e45d6e3d5a869f021561a2434a9b8
Alex Miller <apmiller@covermymeds.com>
2021-04-30 15:04:21 -0400

    Updates CI script

--- build_production.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt-get update
-sudo apt-get install emacs27
-mkdir public
-emacs -Q --batch -l ./publish.el --funcall my/publish
-cp -r site_assets/* public/
