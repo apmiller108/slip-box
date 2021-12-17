7cd8befb7596ef9bef0175fe2560472e84fb314c
Alex Miller <apmiller@covermymeds.com>
2021-12-17 14:33:06 -0600

    Updates build script

--- build_script.sh
+++ build_script.sh
@@ -2,7 +2,19 @@
 
 set -euo pipefail
 
-emacs -Q --batch -l ./publish.el --funcall my/update-org-id-locations-file
+# Copy and compile locked packages
+mkdir -p .packages
+cp -r .packages-locked/* .packages
+emacs -Q --batch -l ./publish.el --funcall my/byte-compile-packages-locked
+
+# Update the org ID locations
+printf 'yes' | emacs -Q --batch -l ./publish.el --funcall my/update-org-id-locations-file
+
+# Build the markup
 emacs -Q --batch -l ./publish.el --funcall my/publish
+
+# Copy the assets (css, js, images, ..etc)
 cp -rf site_assets/* public/
+
+# Build the search index file in public dir
 ruby serialize_search_index.rb | node build-index.js > public/search-index.js
