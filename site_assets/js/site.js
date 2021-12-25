7639ad93d73faa8d63b2f22d4a0c5d912d07b00b
Alex Miller <apmiller@covermymeds.com>
2021-12-25 11:54:47 -0500

    Adds lunr js library for searching

--- site_assets/js/site.js
+++ site_assets/js/site.js
@@ -7,6 +7,7 @@ function ready (callback) {
 };
 
 ready(function() {
+  // Sets the href property of the TOC links.
   document.querySelectorAll('#table-of-contents a').forEach(function(node) {
     var href = "#" + node.textContent.toLowerCase().replace(/\s/g, '-');
     node.setAttribute('href', href);
@@ -15,6 +16,7 @@ ready(function() {
   // Make sure the filter in initialized before trigging clicks
   UIkit.filter('#tag-filter-component');
 
+  // If there's a 'tag' query param, find the link and trigger a click to filter the posts
   var url = new URL(window.location.href);
   var params = new URLSearchParams(url.search);
   var tag = params.get('tag');
@@ -23,4 +25,15 @@ ready(function() {
     var filterId = '#filter-' + tag;
     document.querySelector(filterId).click();
   }
+
+  // Search
+
+  var searchIndex;
+
+  fetch('/js/search-index.json')
+    .then(response => response.json())
+    .then((data) => {
+      searchIndex = lunr.Index.load(data)
+      console.log(searchIndex.search('stacking'));
+    });
 });
