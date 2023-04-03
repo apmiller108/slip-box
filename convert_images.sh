06ca23d7f60d1328ce80f08bb5149801d2cd06a4
Alex Miller <apmiller@covermymeds.com>
2023-04-03 16:25:35 -0500

    Update script

--- convert_images.sh
+++ convert_images.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 mkdir webp # create a new directory to store the converted files
 for file in ./images/*gpt4.jpg; do # loop through all JPG files in the current directory
-  filename=$(basename -- "$file") # get the filename without the extension
-  extension="${filename##*.}" # get the file extension
-  filename="${filename%.*}" # remove the file extension from the filename
   echo "Converting $file to webp..." # print a message to the console
-  mogrify -format webp "$file" -path ./webp/ # convert the file to WebP and save it to the webp directory
+  mogrify -format webp "$file" # convert to webp
+  echo "Converted $file. It is `identify -format "%b\n" $file`" # print completion message and file size
 done
 echo "Conversion complete!" # print a message to the console when all files have been converted
