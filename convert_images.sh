f547daeddf144814b9b6b1715fbeac7034a8ee69
Alex Miller <apmiller@covermymeds.com>
2023-04-11 16:16:16 -0500

    More image optimization

--- convert_images.sh
+++ convert_images.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-mkdir webp # create a new directory to store the converted files
-for file in ./images/*gpt4.jpg; do # loop through all JPG files in the current directory
-  echo "Converting $file to webp..." # print a message to the console
-  mogrify -format webp "$file" # convert to webp
-  echo "Converted $file. It is `identify -format "%b\n" $file`" # print completion message and file size
+set -euo pipefail
+
+for file in ./images/*.jpg ./images/*.jpeg; do 
+  echo "Converting $file to webp..."
+  mogrify -resize '800>' -format webp "$file"
+  echo "Converted $file to webp. It is `identify -format "%b\n" $file`"
+  rm "$file"
 done
-echo "Conversion complete!" # print a message to the console when all files have been converted
+echo "Conversion complete!"
+
+# for file in ./images/*.webp; do
+#   echo "resizing..."
+#   mogrify -resize '800>' "$file"
+#   echo "Resized $file. It is `identify -format "%b\n" $file`"
+# done
+# echo "Conversion complete!" 
